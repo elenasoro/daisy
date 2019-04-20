289ee63c35788b8e8362753a92b4ac27f09d10c0
Elena <Elena@ap-sosnovskayaen.local>
2019-04-20 07:17:37 +0300

    DSY-1: add modal window

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const image = require('gulp-image');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('image', function () {
+    gulp.src('./src/img/*')
+        .pipe(image())
+        .pipe(gulp.dest('./build/img'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('styles/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('styles/css'));
+});
+
+
+gulp.task('fonts', function () {
+    return gulp.src('./src/fonts/*')
+        .pipe(gulp.dest('./build/fonts'))
+});
+
+gulp.task('watch', ['sass', 'image', 'fonts'], function () {
+    gulp.watch('styles/sass/*.scss', ['sass']);
+    gulp.watch('img/*', ['image']);
+    gulp.watch('fonts/*', ['fonts']);
+});
+
+gulp.task('default', ['watch', 'fonts', 'image']);
---
+++ index.js
@@ -0,0 +1,25 @@
+const displayMenu = function(){
+    let menu = document.querySelector('.menu');
+    if (menu.className === "menu") {
+        menu.className += " responsive";
+      } else {
+        menu.className = "menu";
+      }
+}
+
+const openPopup = function(){
+  let buttonOpen = document.querySelector('.get-client');
+  let overlay = document.querySelector('.overlay');
+  
+  buttonOpen.addEventListener('click', overlay.setAttribute('style', 'display:block'));
+  
+}
+
+const closePopup = function(){
+  let buttonClose = document.querySelector('.js-close');
+  let buttonSubmit = document.querySelector('.submit');
+  let overlay = document.querySelector('.overlay');
+
+  buttonClose.addEventListener('click', overlay.setAttribute('style', 'display:none'));
+  buttonSubmit.addEventListener('click', overlay.setAttribute('style', 'display:none'));
+}
